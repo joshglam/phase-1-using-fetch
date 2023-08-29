04cebd8c9aac23974f11c1410b7a5bf0bd9ef453
josh <jglam88@gmail.com>
2023-08-29 17:46:57 -0400

    completed

---
+++ index.js
@@ -0,0 +1,8 @@
+fetch("http://api.open-notify.org/astros.json")
+  .then(function (response) {
+    console.log(response);
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
